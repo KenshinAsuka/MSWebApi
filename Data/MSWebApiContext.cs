f1b72f0ebf71ffc98d9a8b2428091df692c1ae17
BillAng <kenshinasuka@gmail.com>
2022-12-28 20:34:02 +0800

    Add project files.

---
+++ Data/MSWebApiContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MSWebApi;
+
+namespace MSWebApi.Data
+{
+    public class MSWebApiContext : DbContext
+    {
+        public MSWebApiContext (DbContextOptions<MSWebApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MSWebApi.Member> Member { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MSWebApi.Member>().ToTable("Member");
+        }
+    }
+}
---
+++ Member.cs
@@ -0,0 +1,12 @@
+﻿namespace MSWebApi
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        public string MemberName { get; set; } = "";
+
+        public string Details { get; set; } = "";
+
+    }
+}
